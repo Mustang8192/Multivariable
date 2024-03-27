8be4f13fd818a835a46865e3c524e401676688ab
Samy Chirumamilla <107894608+Mustang8192@users.noreply.github.com>
2024-03-27 08:03:49 -0500

    Update Camera2D.gd

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -2,6 +2,5 @@ extends Camera2D
 var smooth_speed = 0.125
 var offset1 = Vector2(0, 0)
 func _ready():
-	pass
-#	var desired_position = get_parent().get_position_delta() + offset1
-#	var smoothed_position = desired_position.lerp(position, smooth_speed)
+	var desired_position = get_parent().get_position_delta() + offset1
+	var smoothed_position = desired_position.lerp(position, smooth_speed)
