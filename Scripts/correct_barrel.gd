8840053e8a7e88ce99ca9cfacc55803676937acb
Samy Chirumamilla <107894608+Mustang8192@users.noreply.github.com>
2024-04-03 08:38:53 -0500

    changed barrel physics and added more floors to the level

--- Scripts/correct_barrel.gd
+++ Scripts/correct_barrel.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 400.0
 
 var direction = -1
 var label
@@ -17,12 +17,20 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y = SPEED + 50
 
-
 	if is_on_floor():
-		if direction:
-			velocity.x = direction * SPEED
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
+		var floor_normal = get_floor_normal()  # Get the normal vector of the floor
+		var slope_angle = atan2(floor_normal.x, floor_normal.y)  # Calculate the angle of the slope
+
+		# Adjust the slope angle to point downward
+		if slope_angle > PI / 2:
+			slope_angle -= PI
+
+		# Calculate the horizontal velocity based on the slope angle
+		velocity.x = SPEED * cos(slope_angle)
+		velocity.y = SPEED * sin(slope_angle)
+
+	else:
+		velocity.x = 0  # No horizontal movement when not on the floor
 
 	move_and_slide()
 
